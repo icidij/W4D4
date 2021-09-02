263653454f241eeef70c051a0e1c4da52ac52db9
rlachivirus <rlachivirus@gmail.com>
2021-09-02 14:52:33 -0400

    two_sum done

--- lib/TDD.rb
+++ lib/TDD.rb
@@ -1,13 +1,19 @@
 class Array
 
-def my_uniq
+    def my_uniq
+        arr = []
+        self.each {|num| arr << num unless arr.include?(num)}
+        arr
+    end
 
-    arr = []
-
-    self.each {|num| arr << num unless arr.include?(num)}
-    arr
-end
-
-
+    def two_sum
+        arr = []
+        self.each_with_index do |num1, i|
+            self.each_with_index do |num2, j|
+                arr << [i, j] if (num1 + num2 == 0) && (i < j)
+            end
+        end
+        arr
+    end
 
 end
--- spec/TDD_spec.rb
+++ spec/TDD_spec.rb
@@ -1,14 +1,33 @@
 require "TDD"
 
-describe "my_uniq" do
-    subject(:array) { [1, 2, 1, 3, 3]}
-    context "when an array is given" do
-        it "should return a new array with unique elements in the order in which they first appeared" do
-            expect(array.my_uniq).to eq([1, 2, 3])
+describe Array do
+
+    describe "my_uniq" do
+        subject(:array) { [1, 2, 1, 3, 3] }
+        context "when an array is given" do
+            it "should return an array with unique elements in the order in which they first appeared" do
+                expect(array.my_uniq).to eq([1, 2, 3])
+            end
+
+            it "should return a new array" do
+                expect(array.my_uniq).to_not be(array)
+            end
+        end
+    end
+
+    describe "#two_sum" do
+        subject(:array) { [-1, 0, 2, -2, 1] }
+        context "when an array is given" do
+            it "finds all pairs of positions where the elements at those positions sum to zero" do
+                expect(array.two_sum).to eq([[0, 4], [2, 3]])
+            end
+
+            it "it sorts pairs from smaller to larger indices" do
+                expect(array.two_sum).to_not eq([[2, 3], [0, 4]])
+                expect(array.two_sum).to_not eq([[4, 0], [3, 2]])
+            end
+
         end
 
-        
     end
-    
-
 end
