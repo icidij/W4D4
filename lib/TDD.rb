2c8e356616f42ce7316b222554ee0e5d57cd68c3
rlachivirus <rlachivirus@gmail.com>
2021-09-02 16:54:03 -0400

    Towers of Hanoi

--- lib/TDD.rb
+++ lib/TDD.rb
@@ -1,43 +1,49 @@
-class Array
+class Towers
 
-    def my_uniq
-        arr = []
-        self.each {|num| arr << num unless arr.include?(num)}
-        arr
-    end
+    # def my_uniq
+    #     arr = []
+    #     self.each {|num| arr << num unless arr.include?(num)}
+    #     arr
+    # end
 
-    def two_sum
-        arr = []
-        self.each_with_index do |num1, i|
-            self.each_with_index do |num2, j|
-                arr << [i, j] if (num1 + num2 == 0) && (i < j)
-            end
-        end
-        arr
-    end
+    # def two_sum
+    #     arr = []
+    #     self.each_with_index do |num1, i|
+    #         self.each_with_index do |num2, j|
+    #             arr << [i, j] if (num1 + num2 == 0) && (i < j)
+    #         end
+    #     end
+    #     arr
+    # end
 
 
-    def my_transpose
-        (0..self.length-1).map do |i|
-            self.map do |subarr|
-                subarr[i]
-            end
-        end
-    end
+    # def my_transpose
+    #     (0..self.length-1).map do |i|
+    #         self.map do |subarr|
+    #             subarr[i]
+    #         end
+    #     end
+    # end
 
     
-    def stock_picker
+    # def stock_picker
 
-        sum = []
-        arr = []
-        self.each_with_index do |num1, i|
-            self.each_with_index do |num2, j|
-                arr << [i, j] if (i < j)
-               sum << (self[j] - self[i]) if i < j
-            end
-        end
+    #     sum = []
+    #     arr = []
+    #     self.each_with_index do |num1, i|
+    #         self.each_with_index do |num2, j|
+    #             arr << [i, j] if (i < j)
+    #            sum << (self[j] - self[i]) if i < j
+    #         end
+    #     end
 
-        arr[sum.index(sum.max)]
+    #     arr[sum.index(sum.max)]
+    # end
+
+    attr_reader :towers
+    def initialize
+        @towers = Array.new(3) { Array.new }
+        @towers[0] = [5, 4, 3, 2, 1]
     end
 
 end
--- spec/TDD_spec.rb
+++ spec/TDD_spec.rb
@@ -2,72 +2,81 @@ require "TDD"
 
 describe Array do
 
-    describe "my_uniq" do
-        subject(:array) { [1, 2, 1, 3, 3] }
-        context "when an array is given" do
-            it "should return an array with unique elements in the order in which they first appeared" do
-                expect(array.my_uniq).to eq([1, 2, 3])
-            end
+    # describe "my_uniq" do
+    #     subject(:array) { [1, 2, 1, 3, 3] }
+    #     context "when an array is given" do
+    #         it "should return an array with unique elements in the order in which they first appeared" do
+    #             expect(array.my_uniq).to eq([1, 2, 3])
+    #         end
 
-            it "should return a new array" do
-                expect(array.my_uniq).to_not be(array)
-            end
-        end
-    end
+    #         it "should return a new array" do
+    #             expect(array.my_uniq).to_not be(array)
+    #         end
+    #     end
+    # end
 
-    describe "#two_sum" do
-        subject(:array) { [-1, 0, 2, -2, 1] }
-        context "when an array is given" do
-            it "finds all pairs of positions where the elements at those positions sum to zero" do
-                expect(array.two_sum).to eq([[0, 4], [2, 3]])
-            end
+    # describe "#two_sum" do
+    #     subject(:array) { [-1, 0, 2, -2, 1] }
+    #     context "when an array is given" do
+    #         it "finds all pairs of positions where the elements at those positions sum to zero" do
+    #             expect(array.two_sum).to eq([[0, 4], [2, 3]])
+    #         end
 
-            it "it sorts pairs from smaller to larger indices" do
-                expect(array.two_sum).to_not eq([[2, 3], [0, 4]])
-                expect(array.two_sum).to_not eq([[4, 0], [3, 2]])
-            end
+    #         it "it sorts pairs from smaller to larger indices" do
+    #             expect(array.two_sum).to_not eq([[2, 3], [0, 4]])
+    #             expect(array.two_sum).to_not eq([[4, 0], [3, 2]])
+    #         end
 
-        end
+    #     end
 
-    end
+    # end
 
-    describe "#my_transpose" do
-        subject(:array) { [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8]
-          ] } 
+    # describe "#my_transpose" do
+    #     subject(:array) { [
+    #         [0, 1, 2],
+    #         [3, 4, 5],
+    #         [6, 7, 8]
+    #       ] } 
 
-        context "when a 2D array is given" do
-            it "should transpose the arrays" do
-                expect(array.my_transpose).to eq( [
-                    [0, 3, 6],
-                    [1, 4, 7],
-                    [2, 5, 8]
-                ])
-            end
+    #     context "when a 2D array is given" do
+    #         it "should transpose the arrays" do
+    #             expect(array.my_transpose).to eq( [
+    #                 [0, 3, 6],
+    #                 [1, 4, 7],
+    #                 [2, 5, 8]
+    #             ])
+    #         end
 
-            it "should return a new 2D array" do
-                expect(array.my_transpose).to_not be(array)
+    #         it "should return a new 2D array" do
+    #             expect(array.my_transpose).to_not be(array)
   
                 
+    #         end
+    #     end
+    # end 
+
+
+    # describe "#stock_picker" do
+    #     subject(:array) {[20, 18, 25, 31, 28, 33]}
+    #     context "when an array with stock prices is given" do
+    #         it "return the most profitable pair of days" do
+    #             expect(array.stock_picker).to eq([1,5])
+    #         end
+    #         it "you can't sell stock before you buy it!" do
+    #             expect(array.stock_picker).to_not eq([5, 1])
+
+    #         end
+    #     end
+    # end
+
+    describe "#initialize" do 
+        subject(:array) { Towers.new }
+        context "when it is instantiated" do
+            it "should create a 2D array" do
+                expect(array.towers).to eq([[5, 4, 3, 2, 1], [], []])                
             end
+            
+            
         end
-    end 
-
-
-    describe "#stock_picker" do
-        subject(:array) {[20, 18, 25, 31, 28, 33]}
-        context "when an array with stock prices is given" do
-            it "return the most profitable pair of days" do
-                expect(array.stock_picker).to eq([1,5])
-            end
-            it "you can't sell stock before you buy it!" do
-                expect(array.stock_picker).to_not eq([5, 1])
-
-            end
-        end
-
-
     end
 end
